02ba4989e9640934e7f3be3a39f452fe9d172a26
AceKatz <elias.h.katz@gmail.com>
2013-10-05 19:16:24 -0400

    Adding stuff for option 5

--- request.h
+++ request.h
@@ -10,141 +10,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_CONCURRENCY 256
 
-struct list_node
-{
-    void *value;
-    struct list_node* next;
-};
 struct request
 {
-    struct list_node* head;
+    void *fd;
+    struct request* next;
 };
+struct request *requests;
 
-
-struct request* ll_create(void)
+struct request* req_create(void *fd)
 {
     struct request *newList = (struct request *) malloc(sizeof(struct request));
     if (newList == NULL)
         return NULL;
-    newList->head = NULL;
+    newList->fd = fd;
+    newList->next = NULL;
     return newList;
 }
-
-void ll_destroy(struct request *ll) 
+struct request *get_request(void) 
 {
-    struct list_node *head = (ll->head);
-    struct list_node *temp = (ll->head->next);
-    while (ll->head != NULL) {
-        free(ll->head);
-        ll->head = temp;
-        if (ll->head == NULL) break;
-        else temp = ll->head->next;
-    }
-}
-
-void ll_add(struct request *ll, void *value) 
-{
-    struct list_node *newNode = (struct list_node*) malloc(sizeof(struct list_node));
-    newNode->value = value;
+    while (requests == NULL) ;
     
-    if (ll->head == NULL) {
-        newNode->next == NULL;
-        ll->head = newNode;
-        
-    } else { 
-        newNode->next = ll->head;
-        ll->head = newNode;
-        
-        
-    }    
+    struct request *ret = requests;
+    requests = requests->next;
+    return ret;
 }
-
-int ll_length(struct request *ll)
+void put_request(struct request *r) 
 {
-    int length = 0;
-    struct list_node *temp;
-    temp = ll->head;
-    while (temp != NULL) {
-        length++;
-        temp = temp->next;
+    if (requests == NULL) {
+        printf("NEW NODE AND SHIT!\n");
+        requests = r;
+        return;
     }
-    return length;
-}
-
-void* ll_remove_first(struct request *ll)
-{
-    struct list_node *temp = ll->head->next;
-    void *retVal = ll->head->value;
-    free(ll->head);
-    ll->head = temp;
-    return retVal;
-    
-    
-}
-
-int ll_remove(struct request *ll, void *value)
-{
-    struct list_node *current = ll->head;
-    struct list_node *next = current->next;
-
-    void *print = current->value;
-    if (print == value) {
-        free(ll->head);
-        ll->head = next;
-        return 0;
-    } else {
-        while (next != NULL) {
-            if (next->value == value) {
-                current->next = next->next;
-                free (next);
-                return 0;
-            } else {
-                current = next;
-                next = next->next;
-            }    
-        }
+    struct request *index = requests;
+    int count = 0;
+    while (index->next != NULL) {
+        index = index->next;
+        count++;
     }
-    return -1;
-        
+    if (count < MAX_CONCURRENCY)
+         index = r;
 }
 
-int ll_contains(struct request *ll, void *value)
-{
-    struct list_node *current = (struct list_node *) malloc(sizeof(struct list_node));
-    struct list_node *next = (struct list_node *) malloc(sizeof(struct list_node));
-    int position = 1;
-    current = ll->head;
-    next = current->next;
-    if (current->value == value) {
-        return position;
-    } else {
-        position++;
-        while (next != NULL) {
-            if (next->value == value) {
-                return position;
-            } else {
-                position++;
-                current = next;
-                next = next->next;
-            }    
-        }
-    }
-    return 0;
-}
 
-void ll_print(struct request *ll) 
-{
-    struct list_node *temp = (struct list_node *) malloc(sizeof(struct list_node));
-    temp = ll->head;
-    if (temp == NULL)
-        printf("This list is empty.  There is nothing to print\n");
-    while (temp != NULL) {
-        int *print = (int *) temp->value;
-        printf("%d\n", print);
-        temp = temp->next;
-    }
-    printf ("\n");
-}
 
 #endif
